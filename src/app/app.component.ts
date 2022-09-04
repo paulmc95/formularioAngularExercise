5c89fbec5d62d020f9d2187a84bb62721b2876b8
Paul Mejía <paul@latamready.com>
2022-09-04 18:39:06 -0500

    Se crea un formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formulario-reto1';
+
+  register={
+    name:'',
+    email:'',
+    password:'',
+  }
+
+
+  onRegister(){
+    console.log(this.register);
+  }
+
 }
